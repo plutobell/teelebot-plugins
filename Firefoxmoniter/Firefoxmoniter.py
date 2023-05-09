92518265021f6f2f1436e7cdfc177929e1831c57
plutobell <53148519+plutobell@users.noreply.github.com>
2023-05-09 20:42:35 +0800

    Update Firefoxmoniter plugin

--- Firefoxmoniter/Firefoxmoniter.py
+++ Firefoxmoniter/Firefoxmoniter.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-import requests, hashlib
+import requests
 from bs4 import BeautifulSoup
 
 def Firefoxmoniter(bot, message):
@@ -8,104 +8,158 @@ def Firefoxmoniter(bot, message):
         h = f.readline()[1:]
     if len(message["text"]) < len(h):
         status = bot.sendChatAction(chat_id=message["chat"]["id"], action="typing")
-        status = bot.sendMessage(chat_id=message["chat"]["id"], text="查询失败！\n邮件地址为空!", parse_mode="HTML", reply_to_message_id=message["message_id"])
+        status = bot.sendMessage(
+            chat_id=message["chat"]["id"],
+            text="查询失败，邮件地址为空。",
+            parse_mode="HTML",
+            reply_to_message_id=message["message_id"]
+        )
         bot.message_deletor(15, message["chat"]["id"], status["message_id"])
         return False
     email = message["text"][len(h)-1:]
     email = email.strip()
     if all([ '@' in email, '.' in email.split('@')[1] ]):
-        ehash = hashlib.sha1(email.encode("utf-8")) #经测试由sha1加密
-        emailhash = ehash.hexdigest()
+        pass
+        # ehash = hashlib.sha1(email.encode("utf-8")) #经测试由sha1加密
+        # emailhash = ehash.hexdigest()
     else:
         status = bot.sendChatAction(chat_id=message["chat"]["id"], action="typing")
-        status = bot.sendMessage(chat_id=message["chat"]["id"], text="查询失败！\n请检查邮件格式!", parse_mode="HTML", reply_to_message_id=message["message_id"])
+        status = bot.sendMessage(
+            chat_id=message["chat"]["id"],
+            text="查询失败，请检查邮件格式。",
+            parse_mode="HTML",
+            reply_to_message_id=message["message_id"]
+        )
         bot.message_deletor(15, message["chat"]["id"], status["message_id"])
         return False
     root_id = bot.root_id
     if str(message["from"]["id"]) == root_id:
         status = bot.sendChatAction(chat_id=message["chat"]["id"], action="typing")
-        status = bot.sendMessage(chat_id=message["chat"]["id"], text="主人，正在查询邮件地址 <b>" + str(email) + " </b>，请稍等...", parse_mode="HTML", reply_to_message_id=message["message_id"])
+        status = bot.sendMessage(
+            chat_id=message["chat"]["id"],
+            text="正在查询邮件地址 <b>" + str(email) + " </b>，请稍等...",
+            parse_mode="HTML",
+            reply_to_message_id=message["message_id"]
+        )
         txt_message_id = status["message_id"]
     else:
         status = bot.sendChatAction(chat_id=message["chat"]["id"], action="typing")
-        status = bot.sendMessage(chat_id=message["chat"]["id"], text="正在查询邮件地址 <b>" + str(email) + " </b>，请稍等...", parse_mode="HTML", reply_to_message_id=message["message_id"])
+        status = bot.sendMessage(
+            chat_id=message["chat"]["id"],
+            text="正在查询邮件地址 <b>" + str(email) + " </b>，请稍等...",
+            parse_mode="HTML",
+            reply_to_message_id=message["message_id"]
+        )
         txt_message_id = status["message_id"]
 
-    ok, protocol, ip, port = get_ip()
-    if ok != True:
-        status = bot.editMessageText(chat_id=message["chat"]["id"],
-            message_id=txt_message_id, text="查询失败！\n获取代理失败，请重试!")
-        bot.message_deletor(15, message["chat"]["id"], status["message_id"])
-        return
-    proxies = {
-        protocol:protocol + '://' + ip + ':' + port
-    }
 
-    url = "https://monitor.firefox.com/"
     r_session = requests.Session()
-    with r_session.get(url, proxies=proxies) as page:
+    # Step 1
+    url = "https://monitor.firefox.com"
+    connect_sid, monitor_x_csrf_token = "", ""
+    with r_session.get(url, timeout=5, verify=False) as page:
         if not page.status_code == requests.codes.ok:
-            status = bot.editMessageText(chat_id=message["chat"]["id"], message_id=txt_message_id, text="查询失败！\n操作过于频繁，请稍后再试!")
+            status = bot.editMessageText(
+                chat_id=message["chat"]["id"],
+                message_id=txt_message_id,
+                text="查询失败，操作过于频繁，请稍后再试。"
+            )
+            bot.message_deletor(15, message["chat"]["id"], status["message_id"])
+            return False
+        connect_sid = r_session.cookies["connect.sid"]
+        monitor_x_csrf_token = page.cookies["monitor.x-csrf-token"]
+
+    # Step 2
+    url = "https://monitor.firefox.com/scan/#email=" + email
+    headers = {
+        'referer': 'https://monitor.firefox.com/',
+        'cookie': 'connect.sid=' + connect_sid + '; monitor.x-csrf-token=' + monitor_x_csrf_token  + ';',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35',
+    }
+    with r_session.get(url, headers=headers, timeout=5, verify=False) as page:
+        if not page.status_code == requests.codes.ok:
+            status = bot.editMessageText(
+                chat_id=message["chat"]["id"],
+                message_id=txt_message_id,
+                text="查询失败，操作过于频繁，请稍后再试。"
+            )
             bot.message_deletor(15, message["chat"]["id"], status["message_id"])
             return False
         page.encoding = "utf-8"
-        connect_sid = page.cookies["connect.sid"]
+        # connect_sid = page.cookies["connect.sid"]
         soup = BeautifulSoup(page.text, "html.parser")
-        csrf = soup.find_all("input")[0]["value"]
+        monitor_x_csrf_token = page.cookies["monitor.x-csrf-token"]
+        x_csrf_token = soup.find('template', {'id':'data'})['data-csrf-token']
         soup.decompose()
 
-    url = "https://monitor.firefox.com/scan"
+    # Step 3
+    url = "https://monitor.firefox.com/api/v1/scan/"
     data = {
-        '_csrf': csrf,
         'email': email,
-        'emailHash': emailhash
     }
     headers = {
-        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
-        'cookie': 'connect.sid=' + connect_sid + "; _ga=GA1.3.1750251620.1566005038; _gid=GA1.3.1329192982.1566005038; _gat=1;",
-        'origin': 'https://monitor.firefox.com',
-        'referer': 'https://monitor.firefox.com/',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+        'Content-Type': 'application/json',
+        'cookie': 'connect.sid=' + connect_sid + '; monitor.x-csrf-token=' + monitor_x_csrf_token  + ';',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35',
+        'x-csrf-token': x_csrf_token,
+        'accept': 'application/json',
     }
-    with r_session.post(url=url, data=data, headers=headers, proxies=proxies, verify=False) as page:
+    with r_session.post(url=url, json=data, headers=headers,
+                        timeout=5, verify=False) as page:
+        if not page.status_code == requests.codes.ok:
+            status = bot.editMessageText(
+                chat_id=message["chat"]["id"],
+                message_id=txt_message_id,
+                text="查询失败，操作过于频繁，请稍后再试。"
+            )
+            bot.message_deletor(15, message["chat"]["id"], status["message_id"])
+            return False
         page.encoding = "utf-8"
-
+        data = []
         result = ""
-        soup = BeautifulSoup(page.text, "html.parser")
+        if page.json().get("success", False):
+            total = page.json().get("total", 0)
+            data = page.json().get("breaches", [])
+            result += "电子邮件地址 <b>" + email +"</b> 出现在 <b>" + str(total) + "</b> 次已知数据外泄事件中。\n\n"
+            for d in data:
+                # name = f'名称: {d["Name"]}\n'
+                title = f'标题: {d["Title"]}\n'
+                domain = f'域名: {d["Domain"]}\n'
+                breach_date = f'泄露时间: {d["BreachDate"].split("T")[0]}\n'
+                added_date = f'记录时间: {d["AddedDate"].split("T")[0]}\n'
+                is_verified_str = "No"
+                is_verified_bool = d["IsVerified"]
+                if is_verified_bool:
+                    is_verified_str = "Yes"
+                is_verified = f'是否验证: {is_verified_str}\n'
+                data_classes =  f'数据类型: {", ".join(d["DataClasses"])}\n'
+                pwn_count = f'数据量: {d["PwnCount"]}\n'
+                # description = f'描述: {d["Description"]}\n'
+                result += "<code>" + title + domain + pwn_count + \
+                    breach_date + added_date + is_verified + data_classes + "</code>\n"
 
-    if "扫描结果" in soup.find("title").text:
-        result += "电子邮件地址 <b>" + email +"</b> 出现在 <b>" + str(soup.find("span", class_="bold").text) + "</b> 次已知数据外泄事件中。\n\n"
-        sections = soup.find_all("div", class_="flx flx-col")
-        if len(sections) > 10:
-            sections = sections[:10]
-        for section in sections:
-            source = section.find_all("span")[0].text + "\n"
-            date = "事件记录时间:\n" + section.find_all("span")[2].text + "\n"
-            data = "泄露的数据:\n" + section.find_all("span")[4].text + "\n"
-            result += source + date + data + "\n"
-        result += "<i>(只显示前10条)</i>"
-
-        soup.decompose()
-        status = bot.editMessageText(chat_id=message["chat"]["id"], message_id=txt_message_id, text=result, parse_mode="HTML")
-        bot.message_deletor(60, message["chat"]["id"], status["message_id"])
-    else:
-        soup.decompose()
-        status = bot.editMessageText(chat_id=message["chat"]["id"], message_id=txt_message_id, text="查询失败！\n请检测命令格式!")
-        bot.message_deletor(15, message["chat"]["id"], status["message_id"])
-
-def get_ip():
-    url = u"http://ip.jiangxianli.com/api/proxy_ip"
-    try:
-        with requests.get(url=url) as req:
-            status = req.json().get("msg")
-            if status == "成功":
-                protocol = req.json().get("data").get("protocol")
-                ip = req.json().get("data").get("ip")
-                port = req.json().get("data").get("port")
-                return True, protocol, ip, port
-    except:
-        return False, False, False, False
+            inlineKeyboard = [
+                [
+                    {"text": "查看更多", "url": "https://monitor.firefox.com/scan/#email=" + email},
+                ]
+            ]
+            reply_markup = {
+                "inline_keyboard": inlineKeyboard
+            }
+            status = bot.editMessageText(
+                chat_id=message["chat"]["id"],
+                message_id=txt_message_id,
+                text=result, parse_mode="HTML",
+                reply_markup=reply_markup,
+                disable_web_page_preview=True
+            )
+            bot.message_deletor(60, message["chat"]["id"], status["message_id"])
+        else:
+            status = bot.editMessageText(
+                chat_id=message["chat"]["id"],
+                message_id=txt_message_id,
+                text="查询失败，请检查命令格式。"
+            )
+            bot.message_deletor(15, message["chat"]["id"], status["message_id"])
 
 
-if __name__ == "__main__":
-    Firefoxmoniter("hi@ojoll.com")
